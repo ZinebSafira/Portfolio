7772cb376ed29c4f3ce2c444db2a1db29741758b
ZinebSafira <84670738+ZinebSafira@users.noreply.github.com>
2024-04-03 10:40:34 +0200

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,14 +1,13 @@
 module.exports = {
-    pathPrefix: "/",
-    siteMetadata: {
-      title: "Zineb Gouram's Portdolio",
-      titleTemplate: "z's portfolio",
-      description:
-        "",
-      url: "https://www.yg.is",
-      image: "/images/logo.png",
-    },
-    plugins: [
-      `gatsby-plugin-transition-link`
-    ]
-  }
+  pathPrefix: "/",
+  siteMetadata: {
+    title: "Zineb Gouram's Portfolio",
+    titleTemplate: "z's portfolio",
+    description: "Description of your portfolio",
+    url: "https://example.com", // Update with your actual URL
+    image: "/images/logo.png",
+  },
+  plugins: [
+    `gatsby-plugin-transition-link`, // Add any additional plugins you need here
+  ]
+}
